85eaa7e6a6a02a2feff3844d8ce5bcde0deeeeed
yuwanlin <yuwanlin@cvte.com>
2023-04-17 20:25:33 +0800

    chore

--- js/util.ts
+++ js/util.ts
@@ -1,30 +1,184 @@
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 import { Stats } from '../lib/util/Stats';
+import * as THREE from 'three';
 export function initStats(type?: any) {
-  var panelType =
-    typeof type !== 'undefined' && type && !isNaN(type) ? parseInt(type) : 0;
-  // @ts-ignore
-  var stats = new Stats();
+    var panelType = typeof type !== 'undefined' && type && !isNaN(type) ? parseInt(type) : 0;
+    // @ts-ignore
+    var stats = new Stats();
 
-  stats.showPanel(panelType); // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.body.appendChild(stats.dom);
+    stats.showPanel(panelType); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(stats.dom);
 
-  return stats;
+    return stats;
 }
 
 function initTrackballControls(camera, renderer) {
-  var trackballControls = new TrackballControls(camera, renderer.domElement);
-  trackballControls.rotateSpeed = 1.0;
-  trackballControls.zoomSpeed = 1.2;
-  trackballControls.panSpeed = 0.8;
-  trackballControls.noZoom = false;
-  trackballControls.noPan = false;
-  trackballControls.staticMoving = true;
-  trackballControls.dynamicDampingFactor = 0.3;
-  // @ts-ignore
-  trackballControls.keys = [65, 83, 68];
+    var trackballControls = new TrackballControls(camera, renderer.domElement);
+    trackballControls.rotateSpeed = 1.0;
+    trackballControls.zoomSpeed = 1.2;
+    trackballControls.panSpeed = 0.8;
+    trackballControls.noZoom = false;
+    trackballControls.noPan = false;
+    trackballControls.staticMoving = true;
+    trackballControls.dynamicDampingFactor = 0.3;
+    // @ts-ignore
+    trackballControls.keys = [65, 83, 68];
 
-  return trackballControls;
+    return trackballControls;
 }
 
-export { initTrackballControls };
+/**
+ * Initialize a simple default renderer and binds it to the "webgl-output" dom
+ * element.
+ *
+ * @param additionalProperties Additional properties to pass into the renderer
+ */
+function initRenderer(additionalProperties) {
+    var props =
+        typeof additionalProperties !== 'undefined' && additionalProperties
+            ? additionalProperties
+            : {};
+    var renderer = new THREE.WebGLRenderer(props);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    document.getElementById('webgl-output').appendChild(renderer.domElement);
+
+    return renderer;
+}
+
+/**
+ * Initialize a simple camera and point it at the center of a scene
+ *
+ * @param {THREE.Vector3} [initialPosition]
+ */
+function initCamera(initialPosition) {
+    var position = initialPosition !== undefined ? initialPosition : new THREE.Vector3(-30, 40, 30);
+
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.copy(position);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    return camera;
+}
+
+function addHouseAndTree(scene) {
+    createBoundingWall(scene);
+    createGroundPlane(scene);
+    createHouse(scene);
+    createTree(scene);
+
+    function createBoundingWall(scene) {
+        var wallLeft = new THREE.BoxGeometry(70, 2, 2);
+        var wallRight = new THREE.BoxGeometry(70, 2, 2);
+        var wallTop = new THREE.BoxGeometry(2, 2, 50);
+        var wallBottom = new THREE.BoxGeometry(2, 2, 50);
+
+        var wallMaterial = new THREE.MeshPhongMaterial({
+            color: 0xa0522d,
+        });
+
+        var wallLeftMesh = new THREE.Mesh(wallLeft, wallMaterial);
+        var wallRightMesh = new THREE.Mesh(wallRight, wallMaterial);
+        var wallTopMesh = new THREE.Mesh(wallTop, wallMaterial);
+        var wallBottomMesh = new THREE.Mesh(wallBottom, wallMaterial);
+
+        wallLeftMesh.position.set(15, 1, -25);
+        wallRightMesh.position.set(15, 1, 25);
+        wallTopMesh.position.set(-19, 1, 0);
+        wallBottomMesh.position.set(49, 1, 0);
+
+        scene.add(wallLeftMesh);
+        scene.add(wallRightMesh);
+        scene.add(wallBottomMesh);
+        scene.add(wallTopMesh);
+    }
+
+    function createGroundPlane(scene) {
+        // create the ground plane
+        var planeGeometry = new THREE.PlaneGeometry(70, 50);
+        var planeMaterial = new THREE.MeshPhongMaterial({
+            color: 0x9acd32,
+        });
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+
+        // rotate and position the plane
+        plane.rotation.x = -0.5 * Math.PI;
+        plane.position.x = 15;
+        plane.position.y = 0;
+        plane.position.z = 0;
+
+        scene.add(plane);
+    }
+
+    function createHouse(scene) {
+        var roof = new THREE.ConeGeometry(5, 4);
+        var base = new THREE.CylinderGeometry(5, 5, 6);
+
+        // create the mesh
+        var roofMesh = new THREE.Mesh(
+            roof,
+            new THREE.MeshPhongMaterial({
+                color: 0x8b7213,
+            })
+        );
+        var baseMesh = new THREE.Mesh(
+            base,
+            new THREE.MeshPhongMaterial({
+                color: 0xffe4c4,
+            })
+        );
+
+        roofMesh.position.set(25, 8, 0);
+        baseMesh.position.set(25, 3, 0);
+
+        roofMesh.receiveShadow = true;
+        baseMesh.receiveShadow = true;
+        roofMesh.castShadow = true;
+        baseMesh.castShadow = true;
+
+        scene.add(roofMesh);
+        scene.add(baseMesh);
+    }
+
+    /**
+     * Add the tree to the scene
+     * @param scene The scene to add the tree to
+     */
+    function createTree(scene) {
+        var trunk = new THREE.BoxGeometry(1, 8, 1);
+        var leaves = new THREE.SphereGeometry(4);
+
+        // create the mesh
+        var trunkMesh = new THREE.Mesh(
+            trunk,
+            new THREE.MeshPhongMaterial({
+                color: 0x8b4513,
+            })
+        );
+        var leavesMesh = new THREE.Mesh(
+            leaves,
+            new THREE.MeshPhongMaterial({
+                color: 0x00ff00,
+            })
+        );
+
+        // position the trunk. Set y to half of height of trunk
+        trunkMesh.position.set(-10, 4, 0);
+        leavesMesh.position.set(-10, 12, 0);
+
+        trunkMesh.castShadow = true;
+        trunkMesh.receiveShadow = true;
+        leavesMesh.castShadow = true;
+        leavesMesh.receiveShadow = true;
+
+        scene.add(trunkMesh);
+        scene.add(leavesMesh);
+    }
+}
+
+export { initTrackballControls, initRenderer, initCamera, addHouseAndTree };
--- my.ts
+++ my.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { initStats, initTrackballControls } from './js/util';
+import { initStats, initRenderer, initCamera, addHouseAndTree } from './js/util';
 // import {
 //     Scene,
 //     PerspectiveCamera,
@@ -23,132 +23,62 @@ import dat from './lib/util/dat.gui';
 
 // listen to the resize events
 function init() {
+    // use the defaults
     var stats = initStats();
+    var renderer = initRenderer();
+    var camera = initCamera();
 
     // create a scene, that will hold all our elements such as objects, cameras and lights.
     var scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0xffffff, 0.015);
-    // scene.overrideMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-    window.three.scene = scene;
 
-    // create a camera, which defines where we're looking at.
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-
-    // create a render and set the size
-    var renderer = new THREE.WebGLRenderer();
-
-    renderer.setClearColor(new THREE.Color(0x000000));
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-
-    // create the ground plane
-    var planeGeometry = new THREE.PlaneGeometry(60, 40, 10, 10);
-    var planeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xffffff,
-    });
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-
-    // rotate and position the plane
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 0;
-    plane.position.y = 0;
-    plane.position.z = 0;
-
-    // add the plane to the scene
-    scene.add(plane);
-
-    // position and point the camera to the center of the scene
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
-    camera.lookAt(scene.position);
-
-    // add subtle ambient lighting
-    var ambientLight = new THREE.AmbientLight(0x3c3c3c);
+    // add ambient lighting
+    var ambientLight = new THREE.AmbientLight('#606008', 1);
     scene.add(ambientLight);
 
     // add spotlight for the shadows
-    var spotLight = new THREE.SpotLight(0xffffff, 1.2, 150, 120);
-    spotLight.position.set(-40, 60, -10);
+    var spotLight = new THREE.SpotLight(0xffffff, 1, 180, Math.PI / 4);
+    spotLight.shadow.mapSize.set(2048, 2048);
+    spotLight.position.set(-30, 40, -10);
     spotLight.castShadow = true;
+    spotLight.shadow.radius = 2;
     scene.add(spotLight);
 
-    // add the output of the renderer to the html element
-    document.getElementById('webgl-output').appendChild(renderer.domElement);
+    // add a simple scene
+    addHouseAndTree(scene);
+
+    // add controls
+    var controls = setupControls();
 
     // call the render function
-    var step = 0;
-
-    var controls = new (function () {
-        this.rotationSpeed = 0.02;
-        this.numberOfObjects = scene.children.length;
-
-        this.removeCube = function () {
-            var allChildren = scene.children;
-            var lastObject = allChildren[allChildren.length - 1];
-            if (lastObject instanceof THREE.Mesh) {
-                scene.remove(lastObject);
-                this.numberOfObjects = scene.children.length;
-            }
-        };
-
-        this.addCube = function () {
-            var cubeSize = Math.ceil(Math.random() * 3);
-            var cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-            var cubeMaterial = new THREE.MeshLambertMaterial({
-                color: Math.random() * 0xffffff,
-            });
-            var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-            cube.castShadow = true;
-            cube.name = 'cube-' + scene.children.length;
-
-            // position the cube randomly in the scene
-
-            cube.position.x = -30 + Math.round(Math.random() * planeGeometry.parameters.width);
-            cube.position.y = Math.round(Math.random() * 5);
-            cube.position.z = -20 + Math.round(Math.random() * planeGeometry.parameters.height);
-
-            // add the cube to the scene
-            scene.add(cube);
-            this.numberOfObjects = scene.children.length;
-        };
-
-        this.outputObjects = function () {
-            console.log(scene.children);
-        };
-    })();
-
-    var gui = new dat.GUI();
-    gui.add(controls, 'rotationSpeed', 0, 0.5);
-    gui.add(controls, 'addCube');
-    gui.add(controls, 'removeCube');
-    gui.add(controls, 'outputObjects');
-    gui.add(controls, 'numberOfObjects').listen();
-
-    // attach them here, since appendChild needs to be called first
-    var trackballControls = initTrackballControls(camera, renderer);
-    var clock = new THREE.Clock();
-
     render();
 
     function render() {
-        trackballControls.update(clock.getDelta());
         stats.update();
-
-        // rotate the cubes around its axes
-        scene.traverse(function (e) {
-            if (e instanceof THREE.Mesh && e != plane) {
-                e.rotation.x += controls.rotationSpeed;
-                e.rotation.y += controls.rotationSpeed;
-                e.rotation.z += controls.rotationSpeed;
-            }
-        });
-
-        // render using requestAnimationFrame
         requestAnimationFrame(render);
         renderer.render(scene, camera);
     }
-}
 
+    function setupControls() {
+        var controls = new (function () {
+            this.intensity = ambientLight.intensity;
+            this.ambientColor = ambientLight.color.getStyle();
+            this.disableSpotlight = false;
+        })();
+
+        var gui = new dat.GUI();
+        gui.add(controls, 'intensity', 0, 3, 0.1).onChange(function (e) {
+            ambientLight.color = new THREE.Color(controls.ambientColor);
+            ambientLight.intensity = controls.intensity;
+        });
+        gui.addColor(controls, 'ambientColor').onChange(function (e) {
+            ambientLight.color = new THREE.Color(controls.ambientColor);
+            ambientLight.intensity = controls.intensity;
+        });
+        gui.add(controls, 'disableSpotlight').onChange(function (e) {
+            spotLight.visible = !e;
+        });
+
+        return controls;
+    }
+}
 export { init };
